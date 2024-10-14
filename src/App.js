9bf54afa70dacc3669a89bf8aa7227c81d868437
Mihir Sawant <mihirssawant06@gmail.com>
2024-10-14 07:29:36 +0530

    logo and nav added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Filters from './components/Filters';
+import Header from './components/Header';
 import Table from './components/Table';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <Router>
       <div>
         {/* Navigation Links */}
-
+        <Header/>
         <Routes>
           <Route path="/" element={<Filters />} />
           <Route path="/table" element={<Table />} />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,99 +1,92 @@
 import React from "react";
 
-const Header = () =>(
-    <div id="page" class="hfeed site">
-  <header id="masthead" class="site-header" role="banner">
-    <div class="wc-inner-header">
-      <a class="skip-link screen-reader-text" href="#site-navigation">Skip to navigation</a>
-      <a class="skip-link screen-reader-text" href="#content">Skip to content</a>
-      
-      <div class="site-branding">
-        <a href="index.html" class="custom-logo-link" rel="home" aria-current="page">
-          <script data-pagespeed-no-defer>
-            (function() {
-              // Script code here to handle critical image loading
-            })();
-          </script>
-          <img 
-            width="291" 
-            height="50" 
-            src="https://office-locator.webbytemplate.com/app/uploads/sites/10/2024/04/Group.png" 
-            class="custom-logo" 
-            alt="Rapnet For WooCommerce" 
-            decoding="async" 
-            data-pagespeed-url-hash="412215313" 
-            onload="pagespeed.CriticalImages.checkImageForCriticality(this);"
-          />
-        </a>
-      </div>
-      
-      <div class="storefront-primary-navigation">
-        <div class="col-full"></div>
-        <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="Primary Navigation">
-          <button id="site-navigation-menu-toggle" class="menu-toggle" aria-controls="site-navigation" aria-expanded="false">
-            <span>Menu</span>
-          </button>
-          
-          <div class="primary-navigation">
-            <ul id="menu-main-menu" class="menu">
-              <li id="menu-item-194" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-194">
-                <a href="https://rapnet.webbytemplate.com/about-us/">About us</a>
-              </li>
-              <li id="menu-item-219" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-219">
-                <a href="https://rapnet.webbytemplate.com/compare/">Compare</a>
-              </li>
-              <li id="menu-item-220" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-220">
-                <a href="https://rapnet.webbytemplate.com/wishlist/">Wishlist</a>
-              </li>
-              <li id="menu-item-212" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-212">
-                <a href="https://rapnet.webbytemplate.com/cart/">Cart</a>
-              </li>
-              <li id="menu-item-213" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-213">
-                <a href="https://rapnet.webbytemplate.com/checkout/">Checkout</a>
-              </li>
-            </ul>
-          </div>
-          
-          <div class="menu">
-            <ul>
-              <li class="current_page_item">
-                <a href="index.html">Home</a>
-              </li>
-              <li class="page_item page-item-8">
-                <a href="https://rapnet.webbytemplate.com/about-us/">About US</a>
-              </li>
-              <li class="page_item page-item-175">
-                <a href="https://rapnet.webbytemplate.com/cart/">Cart</a>
-              </li>
-              <li class="page_item page-item-105">
-                <a href="https://rapnet.webbytemplate.com/checkout/">Checkout</a>
-              </li>
-              <li class="page_item page-item-217">
-                <a href="https://rapnet.webbytemplate.com/compare/">Compare</a>
-              </li>
-              <li class="page_item page-item-19">
-                <a href="https://rapnet.webbytemplate.com/documetation/">Documentation</a>
-              </li>
-              <li class="page_item page-item-35">
-                <a href="https://rapnet.webbytemplate.com/my-account/">My Account</a>
-              </li>
-              <li class="page_item page-item-38 current_page_item">
-                <a href="index.html" aria-current="page">RapNet Shop</a>
-              </li>
-              <li class="page_item page-item-32">
-                <a href="https://rapnet.webbytemplate.com/shop/">Shop</a>
-              </li>
-              <li class="page_item page-item-214">
-                <a href="https://rapnet.webbytemplate.com/wishlist/">Wishlist</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </div>
-  </header>
-</div>
+const Header = () => (
+    <div id="page" className="hfeed site">
+        <header id="masthead" className="site-header" role="banner">
+            <div className="wc-inner-header">
+                <a className="skip-link screen-reader-text" href="#site-navigation">Skip to navigation</a>
+                <a className="skip-link screen-reader-text" href="#content">Skip to content</a>
 
-)
+                <div className="site-branding">
+                    <a href="#" className="custom-logo-link" rel="home" aria-current="page">
+                        {/* Vir Gems logo with link */}
+                        <img 
+                            src="/virgems.png" 
+                            alt="Vir Gems Logo" 
+                            className="diamond-logo diamond-logo-container" 
+                            style={{ maxWidth: '150px', height: 'auto', borderRadius: '50%' }} 
+
+                            decoding="async" 
+                        />
+                    </a>
+                </div>
+
+                <div className="storefront-primary-navigation">
+                    <div className="col-full"></div>
+                    <nav id="site-navigation" className="main-navigation" role="navigation" aria-label="Primary Navigation">
+                        <button id="site-navigation-menu-toggle" className="menu-toggle" aria-controls="site-navigation" aria-expanded="false">
+                            <span>Menu</span>
+                        </button>
+
+                        <div className="primary-navigation">
+                            <ul id="menu-main-menu" className="menu">
+                                <li id="menu-item-194" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-194">
+                                    <a href="https://rapnet.webbytemplate.com/about-us/">About us</a>
+                                </li>
+                                <li id="menu-item-219" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-219">
+                                    <a href="https://rapnet.webbytemplate.com/compare/">Compare</a>
+                                </li>
+                                <li id="menu-item-220" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-220">
+                                    <a href="https://rapnet.webbytemplate.com/wishlist/">Wishlist</a>
+                                </li>
+                                <li id="menu-item-212" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-212">
+                                    <a href="https://rapnet.webbytemplate.com/cart/">Cart</a>
+                                </li>
+                                <li id="menu-item-213" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-213">
+                                    <a href="https://rapnet.webbytemplate.com/checkout/">Checkout</a>
+                                </li>
+                            </ul>
+                        </div>
+                        
+                        <div className="menu">
+                            <ul>
+                                <li className="current_page_item">
+                                    <a href="index.html">Home</a>
+                                </li>
+                                <li className="page_item page-item-8">
+                                    <a href="https://rapnet.webbytemplate.com/about-us/">About US</a>
+                                </li>
+                                <li className="page_item page-item-175">
+                                    <a href="https://rapnet.webbytemplate.com/cart/">Cart</a>
+                                </li>
+                                <li className="page_item page-item-105">
+                                    <a href="https://rapnet.webbytemplate.com/checkout/">Checkout</a>
+                                </li>
+                                <li className="page_item page-item-217">
+                                    <a href="https://rapnet.webbytemplate.com/compare/">Compare</a>
+                                </li>
+                                <li className="page_item page-item-19">
+                                    <a href="https://rapnet.webbytemplate.com/documetation/">Documentation</a>
+                                </li>
+                                <li className="page_item page-item-35">
+                                    <a href="https://rapnet.webbytemplate.com/my-account/">My Account</a>
+                                </li>
+                                <li className="page_item page-item-38 current_page_item">
+                                    <a href="index.html" aria-current="page">RapNet Shop</a>
+                                </li>
+                                <li className="page_item page-item-32">
+                                    <a href="https://rapnet.webbytemplate.com/shop/">Shop</a>
+                                </li>
+                                <li className="page_item page-item-214">
+                                    <a href="https://rapnet.webbytemplate.com/wishlist/">Wishlist</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </div>
+);
 
 export default Header;
